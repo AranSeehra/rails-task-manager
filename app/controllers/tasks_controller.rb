78a8c6429fa3438159b9e7f4f2a6413c4f5efd98
Aran Seehra <aranseehra@gmail.com>
2022-11-15 15:16:48 +0000

    completed app, time to style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def new
@@ -13,8 +15,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(params[:task])
+    task = Task.new(task_params)
     task.save
     redirect_to task_path(task)
+  end
+
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
